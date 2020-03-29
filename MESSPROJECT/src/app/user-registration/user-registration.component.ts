70be644600cf2ea22d63024947f4e7579e6e9879
poojakokate <53407721+poojakokate@users.noreply.github.com>
2020-03-29 20:03:11 +0530

    Second commit

---
+++ MESSPROJECT/src/app/user-registration/user-registration.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.css']
+})
+export class UserRegistrationComponent implements OnInit {
+
+  constructor(private router:Router) { }
+  backButton()
+  {
+    this.router.navigate(['/login']);
+  }
+  ngOnInit() {
+  }
+
+}
+
